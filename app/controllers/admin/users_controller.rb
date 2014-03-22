e424bd89602f81646f8c5ef2129ad86b0acc6f85
gibborjames <gibborjames@gmail.com>
2014-03-22 12:15:48 +0800

    Implement new/create action on users resources [admin]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,24 @@ class Admin::UsersController < Admin::BaseController
   def index
   	@users = User.all
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:notice] = "New user created."
+			redirect_to admin_users_path
+  	else
+  		flash[:alert] = "Error on user."
+			render :action => 'new'
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email, :password)
+  	end
 end
