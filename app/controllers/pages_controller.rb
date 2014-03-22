d9511145f4a7097f6e36292d89f190b08712c1e6
gibborjames <gibborjames@gmail.com>
2014-03-22 11:14:54 +0800

    add condition to pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
 	before_filter :authenticate_user!, :only => [:index]
 
 	def index
-		
+		if current_user.admin?
+			redirect_to admin_root_path
+		end
 	end
 
 	def help
