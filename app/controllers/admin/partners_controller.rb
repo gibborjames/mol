a7ea3ae2344002039c785585f3c8475678f1425e
gibborjames <gibborjames@gmail.com>
2014-03-22 10:13:14 +0800

    Implement create action for partners

--- app/controllers/admin/partners_controller.rb
+++ app/controllers/admin/partners_controller.rb
@@ -1,10 +1,26 @@
 class Admin::PartnersController < Admin::BaseController
 
 	def index
-		
+		@partners = Partner.all
 	end
 
 	def new
 		@partner = Partner.new
 	end
+
+	def create
+		@partner = Partner.new(partner_params)
+		if @partner.save
+			flash[:notice] = "Create new partners."
+			redirect_to admin_partners_path
+		else
+			flash[:alert] = "Error on creating partners."
+			render :action => 'new'
+		end
+	end
+
+	private
+		def partner_params
+			params.require(:partner).permit(:name, :details)
+		end
 end
