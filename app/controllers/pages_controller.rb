3dc28b21ed9ce893325aea79c5356905840bd662
gibborjames <gibborjames@gmail.com>
2014-03-22 12:30:38 +0800

    add partners on header.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 	before_filter :authenticate_user!, :only => [:index]
 
 	def index
+		@partners = Partner.all
 		if current_user.admin?
 			redirect_to admin_root_path
 		end
