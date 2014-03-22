bf82ecbe2b611027ccc140cf206ea58b4746d512
gibborjames <gibborjames@gmail.com>
2014-03-22 12:53:33 +0800

    generate model chassis and column to database.

---
+++ db/migrate/20140322044408_create_chasses.rb
@@ -0,0 +1,22 @@
+class CreateChasses < ActiveRecord::Migration
+  def change
+    create_table :chasses do |t|
+    	t.string :chassis_no
+    	t.string :size
+    	t.string :container_no
+    	t.string :booking_no
+    	t.string :customer
+    	t.string :pull_out_date
+    	t.string :pull_out_time_out
+    	t.string :pull_out_tracker
+    	t.string :pull_out_plate_no
+    	t.string :return_eir_no
+    	t.string :return_date
+    	t.string :return_time_in
+    	t.string :return_tracker
+    	t.string :return_plate_no
+
+      t.timestamps
+    end
+  end
+end
