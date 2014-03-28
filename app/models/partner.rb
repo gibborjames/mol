37256ec0d1d9f37371cedb1939ec4fd48d244297
gibborjames <gibborjames@gmail.com>
2014-03-28 09:37:10 +0800

    add validation uniqueness and presence is true

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,7 @@
 class Partner < ActiveRecord::Base
 
+	validates :name, uniqueness: true, :presence => true
+	validates :details, uniqueness: true, :presence => true
+
 	has_many :items, :dependent => :destroy
 end
