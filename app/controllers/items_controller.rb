e922cd04ac23fbfa8e85e5b5c559df60291d61fd
James Dela Cruz and Robin Bustamante <pair+james+robin@@gmail.com>
2014-04-06 17:58:07 +0800

    Implement update items.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+
+  def show
+    @partners = Partner.all
+    @partner = Partner.find(params[:partner_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @partner = Partner.find(params[:partner_id])
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to [@partner], notice: "SUCCESSFULLY UPDATE."
+    else
+      redirect_to [@partner, @item], notice: "ERROR."
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:chassis_no, :size, :container_no, :customer)
+    end
+end
