ca5193c42f02c9ccc3f0b158ec9eeb58ec245226
gibborjames <gibborjames@gmail.com>
2014-04-06 17:07:40 +0800

    create import method after save for import csv file to item database.

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,6 +1,28 @@
+require 'csv'
+
 class FileUpload < ActiveRecord::Base
 
   has_attached_file :file
-  validates_attachment :file, content_type: { content_type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }
-  # validates_attachment_content_type :file, :content_type => /\Axlsx\/.*\Z/
+  validates_attachment :file, content_type: { content_type: "text/csv" }
+
+  after_create :todo
+
+  private
+    def todo
+      self.save!
+      return import
+    end
+
+    def import
+      file = "public/system/file_uploads/files/000/000/00#{self.id}/original/#{self.file_file_name}"
+      partner = Partner.find(self.partner_id)
+      CSV.foreach(file, headers: true) do |row|
+        partner.items.create!(
+          chassis_no: row[0],
+          size: row[1],
+          container_no: row[2],
+          customer: row[3]
+          )
+      end
+    end
 end
