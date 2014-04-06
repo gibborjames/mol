0451de887491d5816ef01f0bbd4975d30df67fa5
James Dela Cruz and Robin Bustamante <pair+james+robin@@gmail.com>
2014-04-06 17:57:04 +0800

    refactor items to partner

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -3,6 +3,7 @@ class PartnersController < ApplicationController
 	def show
 		@partners = Partner.all
 		@partner = Partner.find(params[:id])
+		@items = @partner.items
 		@file_upload = @partner.file_uploads.build
 	end
 end
