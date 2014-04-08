b0e4db666134895294cf0d77dee75d4d5571eaa2
robin-b <robin.bustamante44@gmail.com>
2014-04-08 16:40:23 +0800

    modified list

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -18,22 +18,22 @@ class FileUpload < ActiveRecord::Base
       partner = Partner.find(self.partner_id)
       CSV.foreach(file, headers: true) do |row|
         # binding.pry
-        partner.items.find_or_create_by_booking_no!(
+        partner.items.find_or_create_by!(
           leasing_company: row[0],
           chassis_no: row[1],
           size: row[2],
           container_no: row[3],
           booking_no: row[4],
           customer: row[5],
-          date_out: row[6],
-          time_out: row[7],
-          trucker: row[8],
-          plate_no: row[9],
-          eir_no: row[10],
-          date_in: row[11],
-          time_in: row[12],
-          trucker_return: row[13],
-          plate_no: row[14]
+          pull_out_date: row[6],
+          pull_out_time_out: row[7],
+          pull_out_tracker: row[8],
+          pull_out_plate_no: row[9],
+          return_eir_no: row[10],
+          return_date: row[11],
+          return_time: row[12],
+          return_trucker: row[13],
+          retured_plate_no: row[14]
           )
       end
     end
